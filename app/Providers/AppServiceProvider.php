f1d72a0aa8ba411319ffc36ef57001f2945652a3
Chinonso Eke <chinonsoeke@gmail.com>
2022-06-28 13:35:48 +0100

    add cors

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,5 +24,7 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    { }
+    {
+        URL::forceScheme('https');
+    }
 }
