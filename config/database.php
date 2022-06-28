e720765a6b23890b909ad8774749d156586a9028
Chinonso Eke <chinonsoeke@gmail.com>
2022-06-28 13:26:04 +0100

    add database

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$database_url = parse_url(env("CLEARDB_DATABASE_URL"));
+
 return [
 
     /*
@@ -42,18 +44,18 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+        
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('DB_HOST', $database_url["host"] ?? '127.0.0.1'),
+            'port' => env('DB_PORT', $database_url["port"] ?? '3306'),
+            'database' => env('DB_DATABASE', ltrim($database_url["path"], "/") ?? 'forge'),
+            'username' => env('DB_USERNAME', $database_url["user"] ?? 'forge'),
+            'password' => env('DB_PASSWORD', $database_url["pass"] ?? ''),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
+            'charset' => 'utf8',
+            'collation' => 'utf8_general_ci',
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
